9cd3e7b85d783b782d61c1f86a56b4606e4a38d2
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2021-12-19 22:39:09 +0100

    otra prueba

--- tests2.js
+++ tests2.js
@@ -3,7 +3,17 @@ var data=fs.readFileSync('./data/biblia.json', 'utf8');
 
 var words=JSON.parse(data);
 var bodyparser=require('body-parser');
+const res = require('express/lib/response');
 
-//console.log(words);
+var libro=Rut;
+var capitulo=0;
 
-console.log(words["Rut"]["chapters"]["0"]);
+var numVer=words[libro]["chapters"][capitulo]["ctd_verses"];
+
+var result="";
+
+for (let step = 1; step <= numVer; step++) {
+    result += words[libro]["chapters"][capitulo]["verses"][step] + "\n";
+}
+
+console.log(result);
